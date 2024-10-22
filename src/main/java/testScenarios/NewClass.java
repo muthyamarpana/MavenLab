fbe692b91cbdb67de6838929b2714f2e438aa497
ArpanaM <muriki.arpana@gmail.com>
2024-10-22 20:35:18 +0530

    code is updated

--- src/main/java/testScenarios/NewClass.java
+++ src/main/java/testScenarios/NewClass.java
@@ -5,6 +5,7 @@ public class NewClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("New Test is created..");
+		System.out.println("Code Updated");
 	}
 
 }
