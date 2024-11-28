ab1bc38f36878b6694181d4609bb69cd4be1ebaa
ArpanaM <muriki.arpana@gmail.com>
2024-11-28 20:59:55 +0530

    Second Commit

---
+++ src/main/java/testScenarios/Test.java
@@ -0,0 +1,12 @@
+package testScenarios;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to git and github...");
+	}
+
+}
